9a0535914f7a72c804f63fe6dbed465291030672
Klinton <klinton.ballecer@gmail.com>
2020-09-15 12:29:12 +0800

    Initial commit

---
+++ template/src/app/store.ts
@@ -0,0 +1,22 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import rootReducer from './rootReducer';
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./rootReducer', () => {
+    const newRootReducer = require('./rootReducer').default;
+    store.replaceReducer(newRootReducer);
+  });
+}
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
